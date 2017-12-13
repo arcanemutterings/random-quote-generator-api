44b9f49039f9284a122baf917d2d59c255323bd3
Nixerrr <nikoszp@gmail.com>
2017-12-13 18:45:20 +0100

    feat JSON improvement

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ CORS(app)
 
 @app.route('/', methods=['GET'])
 def get_quote():
-    return jsonify(getQuote())
+    data=getQuote()
+    print(data)
+    return jsonify(id=data[0][0],
+                   quote=data[0][1],
+                  author=data[0][2])
 
 if __name__ == "__main__":
     app.run()
